2a0cc7419f9526b65fcc278351d93e019c08987d
Sabir Khan <msabirkhan184@gmail.com>
2022-09-03 03:17:21 +0500

    setup

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+      <h1 className="heading">
+        Hi Hakar, Are you ready to do something different?<br/>
+
+        Then why you are here?? <br/>
+
+        Go and do some code!!!
+      </h1>
+   
+  );
+}
+export default App;
