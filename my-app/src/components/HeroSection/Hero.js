efc83c7ba5c41ee78a95ddf611b5561929d46939
Sabir Khan <msabirkhan184@gmail.com>
2022-09-04 00:37:23 +0500

    hero-done

--- my-app/src/components/HeroSection/Hero.js
+++ my-app/src/components/HeroSection/Hero.js
@@ -3,42 +3,57 @@ import './Hero.scss';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 function Hero() {
     return (
         <>
-            <Container className='hero'>
-                <Row>
-                    <Col xl={8} lg={8} md={6} sm={12} xsm={12} >
-                        <div className='connectivity'>
-                            <h1>
-                                Connectivity <br />
-                                <span>
-                                    and Banking <br />
-                                </span>
-                                <span className='billion'>
-                                    for the Next Billion
-                                </span>
-                            </h1>
-                        </div>
-                        <div className='limitless'>
-                            <h2>
-                                Limitless Potential
-                            </h2>
-                            <p>
-                                The world's first blockchain platform that enables individuals and businesses to thrive on Web3 by providing broadband internet connectivity.
-                                <br /><br />
-                                3 billion people cannot access blockchain services because they are not connected to the internet.
-                                We use proprietary technology to build broadband infrastructure where others have failed.
+            <Container fluid className='main-hero'>
+                <Container className='hero'>
+                    <Row>
+                        <Col xl={8} lg={8} md={6} sm={12} xsm={12} >
+                            <div className='connectivity'>
+                                <h1>
+                                    Connectivity <br />
+                                    <span>
+                                        and Banking <br />
+                                    </span>
+                                    <span className='billion'>
+                                        for the Next Billion
+                                    </span>
+                                </h1>
+                            </div>
+                            <div className='limitless'>
+                                <h2>
+                                    Limitless Potential
+                                </h2>
+                                <p>
+                                    The world's first blockchain platform that enables individuals and businesses to thrive on Web3 by providing broadband internet connectivity.
+                                    <br /><br />
+                                    3 billion people cannot access blockchain services because they are not connected to the internet.
+                                    We use proprietary technology to build broadband infrastructure where others have failed.
 
-                            </p>
-                        </div>
-                        
-                    </Col>
-                    <Col lg={4} md={4} sm={12} xsm={12}>
-                        2 of 2
-                    </Col>
-                </Row>
+                                </p>
+                            </div>
+                            <div>
+                                <ButtonGroup className="hero-btns">
+                                    <Button className='litepaper' href='https://forwardprotocol.io' target='_blank'>LITEPAPER
+                                        <img src='assets/whitearrow.svg' alt='arrow' />
+                                    </Button>
+                                    <Button className='asal-join' href='https://forwardprotocol.io' target='_blank'>
+                                        <div className='main-join'>
+                                            <span className='join'>Join our Community</span>
+                                            <img src='assets/gredientarrow.svg' alt='arrow' />
+                                        </div>
+                                    </Button>
+                                </ButtonGroup>
+                            </div>
+                        </Col>
+                        <Col lg={4} md={4} sm={12} xsm={12}>
+                        </Col>
+                    </Row>
+                </Container>
             </Container>
         </>
     );
