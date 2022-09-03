cab8c3add35b6b150f7e170bb67396a8fadc2caa
Sabir Khan <msabirkhan184@gmail.com>
2022-09-04 03:00:45 +0500

    new

--- my-app/src/components/FrictionlessExperience/FrictionlessExperience.js
+++ my-app/src/components/FrictionlessExperience/FrictionlessExperience.js
@@ -4,8 +4,6 @@ import FrictionlessCard from './FrictionlessCard';
 import './FrictionlessExperienceStyle.scss';
 import Stats from './Stats';
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 function FrictionlessExperience() {
   return (
