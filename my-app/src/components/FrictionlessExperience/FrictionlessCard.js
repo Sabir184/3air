edce493d0085d56958390b24cab01eef28d8d036
Sabir Khan <msabirkhan184@gmail.com>
2022-09-03 20:58:18 +0500

    update-overall

--- my-app/src/components/FrictionlessExperience/FrictionlessCard.js
+++ my-app/src/components/FrictionlessExperience/FrictionlessCard.js
@@ -6,17 +6,18 @@ function FrictionlessCard({ CardText, CardImage }) {
   return (
     <>
       <Card className="cardContainer" border="0">
-        <Card.Img style={{ height: '3rem' }} variant="top" src={CardImage}/>
-      <Card bg="transparent" className="cardContainer" border="0">
-        <Card.Img style={{ height: '2.5rem' }} variant="top" src={CardImage} />
-        <Card.Body>
-          <Card.Text style={{ fontSize: '1rem', marginTop: '10px' }}>
-            {CardText}
-          </Card.Text>
-        </Card.Body>
+        <Card.Img style={{ height: '3rem' }} variant="top" src={CardImage} />
+        <Card bg="transparent" className="cardContainer" border="0">
+          <Card.Img style={{ height: '2.5rem' }} variant="top" src={CardImage} />
+          <Card.Body>
+            <Card.Text style={{ fontSize: '1rem', marginTop: '10px' }}>
+              {CardText}
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </Card>
-    </>
-  );
+      </>
+      );
 }
 
-export default FrictionlessCard;
+      export default FrictionlessCard;
