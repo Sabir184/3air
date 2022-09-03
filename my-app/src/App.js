85c1fe32a77c9da1cca078bfb255c145b469266d
Hakar <hakar.yusuf@gmail.com>
2022-09-03 18:35:18 +0300

    added defi & bankind section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,15 @@ import './App.css';
 import Container from 'react-bootstrap/Container';
 import FrictionlessExperience from './components/FrictionlessExperience/FrictionlessExperience';
 import Header from './components/Header/Header';
+import DefiAndBanking from './components/DefiAndBanking/DefiAndBanking';
 
 function App() {
   return (
     <Container>
-      <Header/><br/>
-      <FrictionlessExperience/>
+      <Header />
+      <br />
+      <FrictionlessExperience />
+      <DefiAndBanking />
     </Container>
   );
 }
--- my-app/src/components/FrictionlessExperience/FrictionlessCard.js
+++ my-app/src/components/FrictionlessExperience/FrictionlessCard.js
@@ -5,10 +5,12 @@ import './FrictionlessCard.scss';
 function FrictionlessCard({ CardText, CardImage }) {
   return (
     <>
-      <Card className="cardContainer" border="0">
-        <Card.Img style={{ height: '3rem' }} variant="top" src={CardImage} />
+      <Card bg="transparent" className="cardContainer" border="0">
+        <Card.Img style={{ height: '2.5rem' }} variant="top" src={CardImage} />
         <Card.Body>
-          <Card.Text style={{ marginTop: '1rem' }}>{CardText}</Card.Text>
+          <Card.Text style={{ fontSize: '1rem', marginTop: '10px' }}>
+            {CardText}
+          </Card.Text>
         </Card.Body>
       </Card>
     </>
