af80971b8335683b41a2acdff5cdbdb28f3df012
Hakar <hakar.yusuf@gmail.com>
2022-09-05 12:01:52 +0300

    added animation to header nav

--- my-app/src/components/Header/Header.js
+++ my-app/src/components/Header/Header.js
@@ -27,9 +27,13 @@ export default function Header() {
       <Container className="header">
         <Navbar.Brand href="#">
           {fix ? (
-            <Image src="/assets/airLogoWhite.svg" height={"38px"} width={"94px"} />
+            <Image
+              src="/assets/airLogoWhite.svg"
+              height={'38px'}
+              width={'94px'}
+            />
           ) : (
-            <Image src="/assets/Logo.svg" height={"48px"} width={"114px"} />
+            <Image src="/assets/Logo.svg" height={'48px'} width={'114px'} />
           )}
         </Navbar.Brand>
         <div className="nav-items">
