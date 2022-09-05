c52c30456572cf57d3e45200ce3eecb0177391d5
Hakar <hakar.yusuf@gmail.com>
2022-09-05 14:08:29 +0300

    update

--- my-app/src/components/Loader/Loader.js
+++ my-app/src/components/Loader/Loader.js
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 import './LoaderStyles.scss';
 
 function Loader() {
-  const transition = { duration: 2, yoyo: Infinity, ease: 'easeInOut' };
+  const transition = { duration: 1, yoyo: Infinity, ease: 'easeInOut' };
 
   return (
     <>
